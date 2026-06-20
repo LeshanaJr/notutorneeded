10b87a6abfb03f8d9c2508915834aed139f653c5
LeshanaJr <49703740+LeshanaJr@users.noreply.github.com>
2026-06-20 01:10:06 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBXbAZ9gqOPYZSP1r0QVBs1TaKw_GQ-kDg",
+  authDomain: "notutorneeded.firebaseapp.com",
+  projectId: "notutorneeded",
+  storageBucket: "notutorneeded.firebasestorage.app",
+  messagingSenderId: "722411953569",
+  appId: "1:722411953569:web:707d127d4faa11363cffe0",
+  measurementId: "G-3T7F1WRP54"
 };
